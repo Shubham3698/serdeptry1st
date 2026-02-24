5c7c0b1d1e714b917a14b2bf1bb9c5a7891f322c
shubham3698 <pandey0shubham3698@gmail.com>
2026-02-24 15:06:24 +0530

    Initial commit

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use((err, req, res, next) => {
 // ===================
 // Start Server
 // ===================
-const port = process.env.PORT || 5000; // 🔹 backend now runs on 5000
+const port = process.env.PORT || 3000; // 🔹 backend now runs on 5000
 
 app.listen(port, () => {
   console.log(`🚀 Server running on port ${port}`);
